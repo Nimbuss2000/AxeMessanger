1020815ca15155998f99fbde3745b21946c1ac8f
AL <alexl5105@mail.ru>
2024-06-04 00:34:59 +0300

    Initial Commit

---
+++ AxeMessanger/AxeMessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxeMessangerApp.swift
+//  AxeMessanger
+//
+//  Created by Alexey Larionov on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AxeMessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
